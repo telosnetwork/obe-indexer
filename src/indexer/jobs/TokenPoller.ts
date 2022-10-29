364ac9ec67269520addd278224a02bcf49963eb7
Aaron Cox <aaron@greymass.com>
2022-10-29 14:15:42 -0400

    The fix

--- src/indexer/jobs/TokenPoller.ts
+++ src/indexer/jobs/TokenPoller.ts
@@ -128,6 +128,7 @@ export default class TokenPoller {
             code: token.account,
             scope: `${String(account)} `,
             table: 'accounts',
+            type: AccountRow,
             limit: 200,
         })
 
@@ -139,9 +140,7 @@ export default class TokenPoller {
             )
         } else {
             for (const row of response.rows) {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                const accountRow: AccountRow = row
-                const balance = this.balanceToIntString(accountRow.balance)
+                const balance = String(row.balance.units)
                 const accountStr = account.toString()
                 const query = sql`
                     INSERT INTO balances (token, account, balance)
@@ -152,11 +151,5 @@ export default class TokenPoller {
                 await this.indexer.dbPool?.query(query)
             }
         }
-    }
-
-    private balanceToIntString(balance: string): string {
-        const decimalBalance = balance.split(' ')[0]
-        const intBalance = decimalBalance.replace('.', '').replace(/^0*/, '')
-        return intBalance === '' ? '0' : intBalance
     }
 }
